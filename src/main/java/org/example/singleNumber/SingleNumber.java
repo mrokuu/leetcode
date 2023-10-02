0fae7bd4c792bf430b2eef9200183bb710e6d598
mrokuu <mroczkowskiadammm@gmail.com>
2023-10-02 18:45:42 +0200

    single number

---
+++ src/main/java/org/example/singleNumber/SingleNumber.java
@@ -0,0 +1,21 @@
+package org.example.singleNumber;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for( int el : nums){
+            map.put(el, map.getOrDefault(el, 0)+1);
+        }
+
+        for(Map.Entry<Integer, Integer> el : map.entrySet()){
+            if(el.getValue() == 1){
+                return el.getKey();
+            }
+        }
+    return  -1;
+    }
+}
