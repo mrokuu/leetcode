9ee6245a36f6e26f36b607216e48c8680f0ca5a1
mrokuu <mroczkowskiadammm@gmail.com>
2023-10-04 20:22:00 +0200

    reverse string

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
 
-
+import org.example.reverseString.ReverseString;
 
 public class Main {
     public static void main(String[] args) {
 
+        ReverseString reverseString = new ReverseString();
 
+        System.out.println( reverseString.reverseString("abc"));
     }
 }
