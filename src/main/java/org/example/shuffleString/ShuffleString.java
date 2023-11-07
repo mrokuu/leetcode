ee8de8d7c0ecf2f59e02a5ca2211b4d02329f870
mrokuu <mroczkowskiadammm@gmail.com>
2023-11-07 20:41:23 +0100

    Shuffle String

---
+++ src/main/java/org/example/shuffleString/ShuffleString.java
@@ -0,0 +1,15 @@
+package org.example.shuffleString;
+
+public class ShuffleString {
+
+    public String restoreString(String s, int[] indices) {
+        char [] str = new char[s.length()];
+
+        for(int i = 0; i < s.length(); i++){
+
+            str[indices[i]] = s.charAt(i);
+        }
+
+        return String.valueOf(str);
+    }
+}
