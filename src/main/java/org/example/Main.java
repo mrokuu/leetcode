98782548799f08c52b118f65086e3f1880f2b87d
mrokuu <mroczkowskiadammm@gmail.com>
2023-12-12 14:41:53 +0100

    Remove Duplicates from Sorted Array II

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import org.example.removeDuplicatesfromSortedArrayII.RemoveDuplicatesfromSortedArrayII;
 import org.example.reverseString.ReverseString;
 import org.example.romanToInteger.RomanToInteger;
 import org.example.shuffleString.ShuffleString;
@@ -10,9 +11,8 @@ import org.example.validPalindrome.ValidPalindrome;
 public class Main {
     public static void main(String[] args) {
 
-        ValidAnagram validAnagram = new ValidAnagram();
-        String s = "anagram", t = "nagaram";
-
-        validAnagram.isAnagram(s,t);
+        RemoveDuplicatesfromSortedArrayII removeDuplicatesfromSortedArrayII = new RemoveDuplicatesfromSortedArrayII();
+        int [] array = {0,0,1,1,1,1,2,3,3};
+        removeDuplicatesfromSortedArrayII.removeDuplicates(array);
     }
 }
---
+++ src/main/java/org/example/removeDuplicatesfromSortedArrayII/RemoveDuplicatesfromSortedArrayII.java
@@ -0,0 +1,20 @@
+package org.example.removeDuplicatesfromSortedArrayII;
+
+public class RemoveDuplicatesfromSortedArrayII {
+    public  int removeDuplicates(int[] nums) {
+
+        if(nums.length < 2) return nums.length;
+
+        int j = 2;
+
+        int slow = 2;
+        for (int fast = 2; fast < nums.length; fast++) {
+            if (nums[fast] != nums[slow - 2]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+
+        return slow;
+    }
+}
