90b91e95aee7c17812f4c0176f41388e13e854b3
mrokuu <mroczkowskiadammm@gmail.com>
2023-12-31 16:09:21 +0100

    rotate array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.example.minimumSizeSubarraySum.MinimumSizeSubarraySum;
 import org.example.removeDuplicatesfromSortedArrayII.RemoveDuplicatesfromSortedArrayII;
 import org.example.reverseString.ReverseString;
 import org.example.romanToInteger.RomanToInteger;
+import org.example.rotateArray.RotateArray;
 import org.example.shuffleString.ShuffleString;
 import org.example.validAnagram.ValidAnagram;
 import org.example.validPalindrome.ValidPalindrome;
@@ -12,10 +13,11 @@ import org.example.validPalindrome.ValidPalindrome;
 public class Main {
     public static void main(String[] args) {
 
-        MinimumSizeSubarraySum minimumSizeSubarraySum = new MinimumSizeSubarraySum();
-        int target = 7;
-        int [] nums = {2,3,1,2,4,3};
+        RotateArray rotateArray = new RotateArray();
+        int [] nums = {1,2,3,4,5,6,7};
+        int k = 3;
 
-        System.out.println( minimumSizeSubarraySum.minSubArrayLen(target, nums));
+
+        rotateArray.rotate(nums, k);
     }
 }
