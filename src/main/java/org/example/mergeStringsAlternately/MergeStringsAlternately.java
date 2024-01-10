4d069a49d0ec805a57ffe68df331dccdd6b973e6
mrokuu <mroczkowskiadammm@gmail.com>
2024-01-10 19:39:09 +0100

    Merge Strings Alternately

---
+++ src/main/java/org/example/mergeStringsAlternately/MergeStringsAlternately.java
@@ -0,0 +1,24 @@
+package org.example.mergeStringsAlternately;
+
+public class MergeStringsAlternately {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder merged = new StringBuilder();
+        int i = 0;
+        int j = 0;
+
+        while (i < word1.length() && j < word2.length()) {
+            merged.append(word1.charAt(i++));
+            merged.append(word2.charAt(j++));
+        }
+
+        while (i < word1.length()) {
+            merged.append(word1.charAt(i++));
+        }
+
+        while (j < word2.length()) {
+            merged.append(word2.charAt(j++));
+        }
+
+        return merged.toString();
+    }
+}
